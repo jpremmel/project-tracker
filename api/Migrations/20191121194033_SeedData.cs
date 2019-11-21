aa05815b75d70b71f4d7bde12c5398b203f8ae8b
Ethan Samuels-Ellingson and Jacqueline Remmel <ethan@>
2019-11-21 11:41:26 -0800

    Add seed data

---
+++ api/Migrations/20191121194033_SeedData.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                column: "UserId",
+                value: 1);
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                column: "UserId",
+                value: 2);
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "ProjectId", "Description", "Title", "UserId" },
+                values: new object[,]
+                {
+                    { 1, "This is a description for the first test project. Yay!", "First Test Project", 1 },
+                    { 2, "Here is a description for the second test project.", "Second Test Project", 1 },
+                    { 3, "And finally, a description for the third test project.", "Third Test Project", 1 },
+                    { 4, "This is the first test project for the second user.", "User 2's Test Project", 2 },
+                    { 5, "This is the second test project for the second user.", "Another Test Project", 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Notes",
+                columns: new[] { "NoteId", "Content", "DateTimeString", "ProjectId" },
+                values: new object[,]
+                {
+                    { 1, "Here is the first note!", "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)", 1 },
+                    { 2, "Here is the second note!", "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)", 1 },
+                    { 3, "Here is the third note!", "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)", 1 },
+                    { 4, "Here is the fourth note!", "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)", 2 },
+                    { 5, "Here is the fifth note!", "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)", 5 },
+                    { 6, "Here is the last note!", "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)", 5 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Notes",
+                keyColumn: "NoteId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Notes",
+                keyColumn: "NoteId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Notes",
+                keyColumn: "NoteId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Notes",
+                keyColumn: "NoteId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Notes",
+                keyColumn: "NoteId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Notes",
+                keyColumn: "NoteId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "ProjectId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "ProjectId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "ProjectId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "ProjectId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "ProjectId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2);
+        }
+    }
+}
--- api/Models/ProjectsContext.cs
+++ api/Models/ProjectsContext.cs
@@ -12,13 +12,30 @@ namespace api.Models
     public DbSet<Project> Projects { get; set; }
     public DbSet<Note> Notes { get; set; }
 
-    //SEED DATA
-    // protected override void OnModelCreating(ModelBuilder builder)
-    // {
-    //   builder.Entity<Park>()
-    //       .HasData(
-    //           new Park { ParkId = 1, Name = "Death Valley National Park", Location = "Eastern California; Nevada", SquareMileage = 5262, Terrain = "Harsh desert, salt-flats, sand dunes, badlands, valleys, canyons, mountains", Description = "The hottest, driest and lowest of all the national parks in the United States." },
-    //       );
-    // }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<User>()
+          .HasData(
+              new User { UserId = 1 },
+              new User { UserId = 2}
+          );
+      builder.Entity<Project>()
+          .HasData(
+              new Project { ProjectId = 1, UserId = 1, Title = "First Test Project", Description = "This is a description for the first test project. Yay!" },
+              new Project { ProjectId = 2, UserId = 1, Title = "Second Test Project", Description = "Here is a description for the second test project." },
+              new Project { ProjectId = 3, UserId = 1, Title = "Third Test Project", Description = "And finally, a description for the third test project." },
+              new Project { ProjectId = 4, UserId = 2, Title = "User 2's Test Project", Description = "This is the first test project for the second user." },
+              new Project { ProjectId = 5, UserId = 2, Title = "Another Test Project", Description = "This is the second test project for the second user." }
+          );
+      builder.Entity<Note>()
+          .HasData(
+              new Note { NoteId = 1, ProjectId = 1, Content = "Here is the first note!", DateTimeString = "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)" },
+              new Note { NoteId = 2, ProjectId = 1, Content = "Here is the second note!", DateTimeString = "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)" },
+              new Note { NoteId = 3, ProjectId = 1, Content = "Here is the third note!", DateTimeString = "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)" },
+              new Note { NoteId = 4, ProjectId = 2, Content = "Here is the fourth note!", DateTimeString = "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)" },
+              new Note { NoteId = 5, ProjectId = 5, Content = "Here is the fifth note!", DateTimeString = "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)" },
+              new Note { NoteId = 6, ProjectId = 5, Content = "Here is the last note!", DateTimeString = "Thu Jan 18 2018 13:52:20 GMT-0800 (PST)" }
+          );
+    }
   }
 }
