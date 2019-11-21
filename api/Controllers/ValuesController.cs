179ebd5e190bea093d18b780f21e5964d1bbf03e
Ethan Samuels-Ellingson and Jacqueline Remmel <ethan@>
2019-11-21 13:17:39 -0800

    Add get request to retrieve all projects for a particular user

--- api/Controllers/ValuesController.cs
+++ api/Controllers/ValuesController.cs
@@ -3,43 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        // GET api/values
+        private ProjectsContext _db;
+
+        public ValuesController(ProjectsContext db)
+        {
+            _db = db;
+        }
+
+        //GET api/{userId}
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<List<Project>> Get(int id)
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            var results = _db.Projects.Where(p => p.UserId == id).Include(n => n.Notes).ToList();
+            return results;
         }
     }
 }
